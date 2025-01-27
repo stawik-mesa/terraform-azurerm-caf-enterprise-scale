2c3b9a1b62a6a39fc23550b9f57f77d20d57419b
Stawik, Lukas <stawiklu@mediamarkt.at>
2025-01-27 10:42:48 +0100

    Update provider

--- modules/role_assignments_for_policy/terraform.tf
+++ modules/role_assignments_for_policy/terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.107"
+      version = "~> 4.0"
     }
   }
   required_version = "~> 1.7"
--- terraform.tf
+++ terraform.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~> 3.108"
+      version = "~> 4.0"
       configuration_aliases = [
         azurerm.connectivity,
         azurerm.management,
@@ -19,7 +19,7 @@ terraform {
     }
     azapi = {
       source  = "Azure/azapi"
-      version = "~> 1.13, != 1.13.0"
+      version = "~> 2.0"
     }
   }
   required_version = "~> 1.7"
